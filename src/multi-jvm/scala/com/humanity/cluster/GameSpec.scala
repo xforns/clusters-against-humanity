0aedc4700587e03c60d4d8406e12a71bfc390a1f
Xavier <xavier@getsnipper.com>
2018-02-19 20:48:49 +0100

    pass tests

--- src/multi-jvm/scala/com/humanity/cluster/GameSpec.scala
+++ src/multi-jvm/scala/com/humanity/cluster/GameSpec.scala
@@ -1,5 +1,7 @@
 package com.humanity.cluster
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.Props
 import akka.cluster.Cluster
 import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec}
@@ -12,11 +14,12 @@ import scala.language.postfixOps
 
 object GameSpecConfig extends MultiNodeConfig {
   // register the named roles (nodes) of the test
+  val deck = role("deck")
   val player1 = role("player1")
   val player2 = role("player2")
   val czar = role("czar")
 
-  def nodeList = Seq(player1, player2, czar)
+  def nodeList = Seq(deck, player1, player2, czar)
 
   // Extract individual sigar library for every node.
   nodeList foreach { role =>
@@ -38,15 +41,18 @@ object GameSpecConfig extends MultiNodeConfig {
     akka.remote.artery.enabled = off
     """))
 
-  nodeConfig(player1, player2)(
-    ConfigFactory.parseString("akka.cluster.roles =[player]").withFallback(ConfigFactory.load("game")))
+  nodeConfig(deck)(ConfigFactory.parseString("akka.cluster.roles=[deck]").withFallback(ConfigFactory.load("game")))
 
-  nodeConfig(czar)(ConfigFactory.parseString("akka.cluster.roles =[czar]").withFallback(ConfigFactory.load("game")))
+  nodeConfig(player1, player2)(
+    ConfigFactory.parseString("akka.cluster.roles=[player]").withFallback(ConfigFactory.load("game")))
+
+  nodeConfig(czar)(ConfigFactory.parseString("akka.cluster.roles=[czar]").withFallback(ConfigFactory.load("game")))
 }
 
 class GameSpecMultiJvmNode1 extends GameSpec
 class GameSpecMultiJvmNode2 extends GameSpec
 class GameSpecMultiJvmNode3 extends GameSpec
+class GameSpecMultiJvmNode4 extends GameSpec
 
 abstract class GameSpec extends MultiNodeSpec(GameSpecConfig)
   with WordSpecLike with Matchers with BeforeAndAfterAll with ImplicitSender {
@@ -60,25 +66,34 @@ abstract class GameSpec extends MultiNodeSpec(GameSpecConfig)
   override def afterAll() = multiNodeSpecAfterAll()
 
   "The game" must {
+
+    "illustrate how to start the deck" in within(15 seconds) {
+      runOn(deck) {
+        Cluster(system) join node(deck).address
+        val deckActor = system.actorOf(Props[Deck], name = "deck")
+        deckActor ! DeckQuestion(null)
+        //expectNoMessage(new FiniteDuration(5,TimeUnit.SECONDS))
+        import scala.concurrent.duration._
+        expectMsgClass(20.seconds, classOf[Question])
+      }
+      testConductor.enter("deck-started")
+    }
+
     "illustrate how to start the first player" in within(15 seconds) {
       runOn(player1) {
         Cluster(system) join node(player1).address
         val player = system.actorOf(Props[Player], name = "player")
-        player ! Question("this is a question")
-        expectMsgPF() {
-          case _:Answer =>
-        }
+        player ! StartGameRound(null)
+        import scala.concurrent.duration._
+        expectNoMessage(5.seconds)
       }
-
-      // this will run on all nodes
-      // use barrier to coordinate test steps
       testConductor.enter("player1-started")
     }
 
     "illustrate how the czar automatically registers" in within(15 seconds) {
       runOn(czar) {
         Cluster(system) join node(player1).address
-        system.actorOf(Props(classOf[Czar],true), name = "czar")
+        system.actorOf(Props(classOf[Czar],2), name = "czar")
       }
       testConductor.enter("czar-started")
 
@@ -89,19 +104,17 @@ abstract class GameSpec extends MultiNodeSpec(GameSpecConfig)
       testConductor.enter("player-czar-ok")
     }
 
-    "illustrate how more nodes register" in within(20 seconds) {
+    "illustrate how more nodes register" in within(15 seconds) {
       runOn(player2) {
         Cluster(system) join node(player1).address
         system.actorOf(Props[Player], name = "player")
       }
       testConductor.enter("player2-started")
-
       testConductor.enter("all-started")
 
       runOn(player1, player2) {
         assertServiceOk()
       }
-
       testConductor.enter("all-ok")
     }
 
@@ -110,8 +123,9 @@ abstract class GameSpec extends MultiNodeSpec(GameSpecConfig)
   def assertServiceOk(): Unit = {
     val player = system.actorSelection("akka://" + system.name + "/user/player")
     awaitAssert {
-      player ! Question("this is a question")
-      expectMsgType[Answer](1.second).content should not be empty
+      player ! StartGameRound(null)
+      import scala.concurrent.duration._
+      expectNoMessage(5.seconds)
     }
   }
 
