671d35e1e78627f765362cc1c315d35c429062e3
Xavier <xavier@getsnipper.com>
2018-02-24 21:39:54 +0100

    refactor gameSpec tests

--- src/multi-jvm/scala/com/humanity/cluster/GameSpec.scala
+++ src/multi-jvm/scala/com/humanity/cluster/GameSpec.scala
@@ -1,12 +1,14 @@
 package com.humanity.cluster
 
-import akka.actor.Props
+import akka.actor.{ActorNotFound, ActorRef, Props}
 import akka.cluster.Cluster
 import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec}
 import akka.testkit.ImplicitSender
+import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
+import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
@@ -63,67 +65,139 @@ abstract class GameSpec extends MultiNodeSpec(GameSpecConfig)
 
   override def afterAll() = multiNodeSpecAfterAll()
 
+  val deckAddress = node(deck).address
+  val player1Address = node(player1).address
+  val player2Address = node(player2).address
+  val czarAddress = node(czar).address
+
   "The game" must {
 
     "illustrate how to start the deck" in within(4 seconds) {
       runOn(deck) {
-        Cluster(system) join node(deck).address
-        val deckActor = system.actorOf(Props[Deck], name = "deck")
+        Cluster(system) join deckAddress
+        val deckActor = getOrCreateDeckActor.get
+        deckActor ! RestartGame
         deckActor ! DeckQuestion(null)
+
         import scala.concurrent.duration._
         expectMsgClass(2.seconds, classOf[Question])
+
+        Cluster(system).leave(deckAddress)
       }
       testConductor.enter("deck-started")
     }
 
     "illustrate how to start the first player" in within(4 seconds) {
       runOn(player1) {
-        Cluster(system) join node(player1).address
-        val player = system.actorOf(Props[Player], name = "player")
+        Cluster(system) join player1Address
+        val player = getOrCreatePlayerActor("1").get
+        player ! RestartGame
         player ! StartGameRound(null)
+
         import scala.concurrent.duration._
         expectNoMessage(2.seconds)
+
+        Cluster(system).leave(player1Address)
       }
       testConductor.enter("player1-started")
     }
 
     "illustrate how the czar automatically registers" in within(4 seconds) {
       runOn(czar) {
-        Cluster(system) join node(player1).address
-        system.actorOf(Props(classOf[Czar],2), name = "czar")
+        Cluster(system) join czarAddress
+        getOrCreateCzarActor().get
       }
       testConductor.enter("czar-started")
 
       runOn(player1) {
-        assertServiceOk()
+        val player = getOrCreatePlayerActor("1").get
+        player ! RestartGame
+        player ! StartGameRound(null)
+
+        import scala.concurrent.duration._
+        expectNoMessage(2.seconds)
+
+        Cluster(system).leave(czarAddress)
       }
 
       testConductor.enter("player-czar-ok")
     }
 
     "illustrate how more nodes register" in within(4 seconds) {
-      runOn(player2) {
-        Cluster(system) join node(player1).address
-        system.actorOf(Props[Player], name = "player")
+      runOn(player1) {
+        Cluster(system) join player1Address
+        getOrCreatePlayerActor("1").get
       }
-      testConductor.enter("player2-started")
-      testConductor.enter("all-started")
+      testConductor.enter("player1-entered")
 
-      runOn(player1, player2) {
-        assertServiceOk()
+      runOn(player2) {
+        Cluster(system) join player2Address
+        getOrCreatePlayerActor("2").get
+      }
+      testConductor.enter("player2-entered")
+
+      runOn(player1,player2) {
+        val player = getOrCreatePlayerActor("1").get
+
+        val player2 = getOrCreatePlayerActor("2").get
+
+        player ! RestartGame
+        player ! StartGameRound(null)
+
+        import scala.concurrent.duration._
+        expectNoMessage(2.seconds)
+
+        player2 ! RestartGame
+        player2 ! StartGameRound(null)
+
+        //import scala.concurrent.duration._
+        //expectNoMessage(2.seconds)
+
+        Cluster(system).leave(player1Address)
+        Cluster(system).leave(player2Address)
       }
       testConductor.enter("all-ok")
     }
 
   }
 
-  def assertServiceOk(): Unit = {
-    val player = system.actorSelection("akka://" + system.name + "/user/player")
-    awaitAssert {
-      player ! StartGameRound(null)
-      import scala.concurrent.duration._
-      expectNoMessage(2.seconds)
+  def getOrCreateDeckActor(): Option[ActorRef] = {
+    implicit val duration: Timeout = 200 millis
+    val actorRefFuture = system.actorSelection("akka://" + system.name + "/user/deck").resolveOne()
+    var ref:Option[ActorRef] = None
+    try {
+      ref = Some(Await.result(actorRefFuture,200.millis))
+    } catch{
+      case an:ActorNotFound => ref = Some(system.actorOf(Props[Deck], name = "deck"))
     }
+
+    return ref
+  }
+
+  def getOrCreatePlayerActor(number:String): Option[ActorRef] = {
+    implicit val duration: Timeout = 200 millis
+    val actorRefFuture = system.actorSelection("akka://" + system.name + "/user/player"+number).resolveOne()
+    var ref:Option[ActorRef] = None
+    try {
+      ref = Some(Await.result(actorRefFuture,200.millis))
+    } catch{
+      case an:ActorNotFound => ref = Some(system.actorOf(Props[Player], name = "player"+number))
+    }
+
+    return ref
+  }
+
+  def getOrCreateCzarActor(): Option[ActorRef] = {
+    implicit val duration: Timeout = 200 millis
+    val actorRefFuture = system.actorSelection("akka://" + system.name + "/user/czar").resolveOne()
+    var ref:Option[ActorRef] = None
+    try {
+      ref = Some(Await.result(actorRefFuture,200.millis))
+    } catch{
+      case an:ActorNotFound => ref = Some(system.actorOf(Props(classOf[Czar],2), name = "czar"))
+    }
+
+    return ref
   }
 
 }
